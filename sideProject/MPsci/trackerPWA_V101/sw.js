3208002fa9b8d4294fe79d788150869d1d162395
michaelpig <pig7034392@gmail.com>
2024-11-01 00:55:28 +0800

    Site updated: 2024-11-01 00:55:22

---
+++ sideProject/MPsci/trackerPWA_V101/sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'motion-tracker-v1';
+const urlsToCache = [
+    '../tracker_V101.html',
+    './manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+    './icon-48x48.png',
+    './icon-72x72.png',
+    './icon-96x96.png',
+    './icon-144x144.png',
+    './icon-192x192.png',
+    './icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request)
+                    .then(response => {
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    })
+                    .catch(() => {
+                        if (event.request.mode === 'navigate') {
+                            return caches.match('./tracker_V101.html');
+                        }
+                    });
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+}); 
